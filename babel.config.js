69c3ed90cc4db2902549fa886b87b91ce94a2e9f
ironreplica <trevorchilds8@gmail.com>
2024-12-09 19:51:48 -0700

    Fixed Compile Error with babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  resolve: {
-    fallback: {
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      stream: require.resolve("stream-browserify"),
-      zlib: require.resolve("browserify-zlib"),
-      timers: require.resolve("timers-browserify"),
-    },
-  },
 };
